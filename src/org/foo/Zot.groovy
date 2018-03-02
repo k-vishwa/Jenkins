6fd010ec6ba76c5050dd483ec5548cb1cee86f82
k-vishwa <vishwanath.k@kloud9.nyc>
2018-03-02 09:04:07 +0530

    Modify Jenkinsfile

--- src/org/foo/Zot.groovy
+++ src/org/foo/Zot.groovy
@@ -1,11 +1,11 @@
 package org.foo;
 
-def checkOutFrom(repo) {
-  git url: "git@github.com:jenkinsci/${repo}"
-}
 
-// return this
 
 class Zot {
+    def checkOutFrom(repo) {
+    git url: "git@github.com:jenkinsci/${repo}"
+    }
 
+    return this
 }
