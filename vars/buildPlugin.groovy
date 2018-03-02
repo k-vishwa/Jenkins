e8e0a8ea79193c428795923eca98c2812770ecd9
k-vishwa <vishwanath.k@kloud9.nyc>
2018-03-02 10:22:47 +0530

    Modify Jenkinsfile

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -1,8 +1,24 @@
-def call(Map config) {
+// def call(Map config) {
+//     node {
+//         /* git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
+//         sh 'mvn install'
+//          mail to: '...', subject: "${config.name} plugin build", body: '...' */
+//         echo 'in BUild Plugin' ${config.name}
+//     }
+// }
+
+
+def call(body) {
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
     node {
         /* git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
         sh 'mvn install'
          mail to: '...', subject: "${config.name} plugin build", body: '...' */
-        echo 'in BUild Plugin' ${config.name}
+        echo 'in BUild Plugin' config.name
     }
-}
+}    
